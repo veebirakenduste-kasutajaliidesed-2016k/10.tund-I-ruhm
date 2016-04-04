866f1e7713e1531284b46550204992a32c8c7eba
romil rõbtšenkov <romilrobtsenkov@gmail.com>
2016-04-04 12:37:19 +0300

    added php api

--- getFeed.php
+++ getFeed.php
@@ -1,12 +1,23 @@
 <?php
-//getFeed.php?search=%23Tallinn
+  //getFeed.php?search=%23Tallinn
 
-//https://github.com/J7mbo/twitter-api-php
-require_once("twitterAPIExchange.php");
+  //https://github.com/J7mbo/twitter-api-php
+  require_once("twitterAPIExchange.php");
 
-require_once("config.php");
+  require_once("config.php");
 
+  $url = "https://api.twitter.com/1.1/search/tweets.json";
+  $getField = "?q=%23Tallinn&result_type=recent";
 
+  $requestMethod = "GET";
 
+  //$config tuleb config.php failist
+  $twitter = new twitterAPIExchange($config);
+
+  $dataFromAPI = $twitter->setGetfield($getField)
+               ->buildOauth($url, $requestMethod)
+               ->performRequest();
+
+  echo $dataFromAPI;
 
 ?>
