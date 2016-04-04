6f1f029dd5c34073f7b1a3692ee3396f706983ba
romil rõbtšenkov <romilrobtsenkov@gmail.com>
2016-04-04 12:58:53 +0300

    added cache file usage

--- getFeed.php
+++ getFeed.php
@@ -11,6 +11,17 @@
 
   $requestMethod = "GET";
 
+
+  $file_name = "cache.txt";
+  $data_json = file_get_contents("cache.txt");
+  $data = json_decode($data_json);
+
+
+  if(strtotime(date('c')) - strtotime($data->date_written) > 10){
+    // kui rohkem kui 10s möödas lae uuesti, muidu näita välja mis oli failis
+  }
+
+
   //$config tuleb config.php failist
   $twitter = new twitterAPIExchange($config);
 
@@ -18,6 +29,15 @@
                ->buildOauth($url, $requestMethod)
                ->performRequest();
 
-  echo $dataFromAPI;
+  //echo $dataFromAPI;
+
+  $o = new StdClass();
+  $o->date_written = date('c');
+  $o->api = $dataFromAPI;
+
+  //teen objekti stringiks ja salvestan faili
+  file_put_contents($file_name, json_encode($o));
+
+  echo json_encode($o);
 
 ?>
